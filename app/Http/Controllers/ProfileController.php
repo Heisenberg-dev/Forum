6d7ece91ae8f47723ca69c5e894b5bf4a0f4e021
Heisenberg-dev <arthur.bagrov@gmail.com>
2024-08-24 00:05:41 +0300

    fixed profile controller errors

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,9 +13,14 @@ use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
-    /**
-     * Display the user's profile form.
-     */
+    public function show(Request $request): View
+    {
+        return view('profile.show', [
+            'user' => $request->user(),
+        ]);
+    }
+
+
     public function edit(Request $request): View
     {
         return view('profile.edit', [
